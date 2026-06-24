818037b63e65cbeb1268a02d7e7ff03d27b4c45e
Gourish Prajapati <gourishp18@gmail.com>
2026-06-24 23:41:27 +0530

    Testing ReviewPilot

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react"
 const useCurrencyInfo = (currency) => {
   const [data, setData] = useState({});
-  console.log("ReviewPilot test");
+  console.log("ReviewPilot test1");
+  console.log("ReviewPilot test2");
+
   useEffect(() => {
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-06-11/v1/currencies/${currency}.json`)
       .then((res) => res.json())

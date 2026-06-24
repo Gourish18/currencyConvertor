ed9f0a7525eac4ba20d55a5175e3544f8ff53fce
Gourish Prajapati <gourishp18@gmail.com>
2026-06-24 23:45:52 +0530

    Testing PR webhook

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 const useCurrencyInfo = (currency) => {
   const [data, setData] = useState({});
   console.log("ReviewPilot test");
-  console.log("ReviewPilot test2");
+  console.log("ReviewPilot PR Test");
 
   useEffect(() => {
     fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2025-06-11/v1/currencies/${currency}.json`)
